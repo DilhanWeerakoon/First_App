4a74e9e112b1c1fb2bfb2a2aae157348c607810c
Dilhan_Weerakoon <weerakoondilshan.dw@gmail.com>
2020-09-10 22:12:00 +0530

    First Commit

--- app/src/main/java/com/example/first_app/Login.java
+++ app/src/main/java/com/example/first_app/Login.java
@@ -2,14 +2,19 @@ package com.example.first_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
+
+
+
 public class Login extends AppCompatActivity {
-    Button callSingUp;
+    Button callSignUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +24,15 @@ public class Login extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
-        callSingUp = findViewById(R.id.singup_screen);
 
-        callSingUp.setOnClickListener(new View.OnClickListener() {
+        callSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Login.this,SingUp.class);
                 startActivity(intent);
                 finish();
+
+
             }
         });
     }
